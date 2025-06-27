8f2879176fe2d18b66b14640e59e898ba51ea5d1
Stephen <stephenmburumburu82@gmail.com>
2025-06-27 14:26:51 +0300

    phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the existing <main> element
+const main = document.getElementById('main');
+main.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set its id to "victory"
+newHeader.id = 'victory';
+
+// Set the inner text
+newHeader.textContent = "Stephen Mburu is the champion";
+
+// Append the new header to the body
+document.body.append(newHeader);
